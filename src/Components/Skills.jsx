00eaf5fd75f97e303217fb39cfe21746df5479c6
Ayush Yadav <sidharth197502@gmail.com>
2024-08-13 19:44:41 +0530

    updated the years to 2 in skills sections

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,17 +8,17 @@ const Skills = () => {
         Skills
       </h2>
       <div className="border-2 rounded-3xl border-gray-700 md:px-10 overflow-hidden w-full ">
-        <SkillsTemp name="HTML" image={html} year="1.5 years" />
+        <SkillsTemp name="HTML" image={html} year="2 years" />
         <SkillsTemp
           name="CSS"
           image="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
-          year="1.5 years"
+          year="2 years"
         />
 
         <SkillsTemp
           name="Java Script"
           image="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
-          year="1.5 years"
+          year="2 years"
         />
 
         <SkillsTemp
@@ -30,7 +30,7 @@ const Skills = () => {
         <SkillsTemp
           name="Firebase"
           image="https://upload.wikimedia.org/wikipedia/commons/8/85/Firebase.svg"
-          year="1 year"
+          year="1.5 year"
         />
 
         <SkillsTemp
