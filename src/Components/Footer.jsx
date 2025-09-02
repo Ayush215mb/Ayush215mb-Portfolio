9a15de449cfbc864d86f8f87dabaea1a001ba614
Ayush Yadav <sidharth197502@gmail.com>
2025-09-02 13:10:35 +0530

    made the footer responsive

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ import twitterlogo from "../assets/Images/twitter.jpg";
 const Footer = () => {
   return (
     <div className="flex my-10  md:mx-40 items-center justify-center  ">
-      <div className="flex items-center justify-around gap-7">
+      <div className="flex items-center justify-around md:gap-7 gap-6">
         <a href="https://www.linkedin.com/in/ayush215mb/" target="_blank">
           <motion.img
             variants={fadeIn("up", 0.2)}
@@ -14,7 +14,7 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.7 }}
             src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png"
             alt="Linkedin_logo"
-            className="max-w-12"
+            className="md:max-w-12 max-w-8"
           />
         </a>
         <a href="https://www.instagram.com/ayush215mb" target="_blank">
@@ -25,7 +25,7 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.7 }}
             src=" https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png"
             alt="Insta_logo"
-            className="max-w-12"
+            className="md:max-w-12 max-w-8"
           />
         </a>
         <a href="https://github.com/Ayush215mb" target="_blank">
@@ -36,7 +36,7 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.7 }}
             src={githublogo}
             alt="Github_logo"
-            className="max-w-12 "
+            className="md:max-w-12 max-w-8 "
           />
         </a>
 
@@ -48,7 +48,7 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.7 }}
             src="https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-02-512.png"
             alt="gmail_logo"
-            className="max-w-12 "
+            className="md:max-w-12 max-w-8 "
           />
         </a>
         <a href="https://discord.com/users/903294131768545291" target="_blank">
@@ -59,18 +59,18 @@ const Footer = () => {
             viewport={{ once: true, amount: 0.7 }}
             src=" https://cdn0.iconfinder.com/data/icons/free-social-media-set/24/discord-64.png"
             alt="discord_logo"
-            className="max-w-12 "
+            className="md:max-w-12 max-w-8 "
           />
         </a>
         <a href="https://x.com/glevoidd" target="_blank">
           <motion.img
-            variants={fadeIn("up", 0.6)}
+            variants={fadeIn("up", 0.7)}
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: true, amount: 0.7 }}
             src={twitterlogo}
             alt="twitter_logo"
-            className="max-w-12 "
+            className="md:max-w-12 max-w-8 "
           />
         </a>
       </div>
