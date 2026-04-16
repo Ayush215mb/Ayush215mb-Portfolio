c256d93cc478b1943b847efa2569b15013d50fdc
Ayush Yadav <sidharth197502@gmail.com>
2026-04-16 15:40:47 +0530

    building it in next js

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>Ayush Yadav</p>
+    </div>
+  );
+}
--- public/Images/source.js
+++
@@ -1,3 +0,0 @@
-//https://favicon.io/emoji-favicons/money-with-wings
-
-//this is the source of all favicons
